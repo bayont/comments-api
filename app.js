f731ebb5cd1bef50a2d4b9a5161f2c4c4b520477
bayont <f.fetter02@gmail.com>
2022-12-03 14:14:00 +0100

    feat: add cors headers

--- app.js
+++ app.js
@@ -7,8 +7,25 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Importing routes from dir ./routes
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use('/', routes);
-console.log(`Listening: http://localhost:${process.env.APP_PORT || '3000'}/`);
+console.log(`Listening: http://localhost:${process.env.APP_PORT || '3001'}/`);
 
 module.exports = app;
