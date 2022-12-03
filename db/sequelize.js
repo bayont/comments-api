7501aa18651160d2c6c61bc750f6ef62c127c073
bayont <f.fetter02@gmail.com>
2022-12-03 14:16:00 +0100

    feat: add dotenv globally

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,13 +1,9 @@
 import { Sequelize } from "sequelize";
 
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-
 const connectionString = process.env.MYSQL_CONNECTION_STRING;
 if (!connectionString) {
     console.error("Provide correct MYSQL_CONNECTION_STRING in .env file");
     process.exit(0)
 }
 
-export const sequelize = new Sequelize(process.env.MYSQL_CONNECTION_STRING);
+export const sequelize = new Sequelize(process.env.MYSQL_CONNECTION_STRING, { logging: false });
